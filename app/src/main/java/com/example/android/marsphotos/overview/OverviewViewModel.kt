840b08f29825048084f7ad1fb7a3478a6af30276
KelechiDanAndroid <kcfordskys@yahoo.com>
2021-07-05 03:07:49 +0200

    fifth commit

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -37,7 +37,11 @@ class OverviewViewModel : ViewModel() {
     // The external immutable LiveData for the request status
     val status: LiveData<MarsApiStatus> = _status
 
+    // Internally, we use a MutableLiveData, because we will be updating the List of MarsPhoto
+    // with new values
     private val _photos = MutableLiveData<List<MarsPhoto>>()
+
+    // The external LiveData interface to the property is immutable, so only this class can modify it
     val photos: LiveData<List<MarsPhoto>> = _photos
 
     /**
